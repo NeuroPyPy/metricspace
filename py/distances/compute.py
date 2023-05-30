7cdd5e15fbde79f451e7e2a6ca9efea91ce79f54
NeuroPyPy <Flynnoconnell@gmail.com>
2023-05-30 17:08:17 -0400

    incorperate metric space analysis

--- py/distances/compute.py
+++ py/distances/compute.py
@@ -1,23 +1,23 @@
 import os
+import numpy as np
 import scipy.io as sio
 import matlab
 import matlab.engine
 
-def pairwise_distances(cspks):
+def dist_engine(cspks) -> np.ndarray:
     try:
         eng = matlab.engine.connect_matlab(matlab.engine.find_matlab()[0])
         print("Connected to existing MATLAB session")
     except IndexError:
         eng = matlab.engine.start_matlab()
         print("Started new MATLAB session")
-    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..' ))
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..' ))
     eng.addpath(os.path.join(root_path, 'mat'))  # Add the 'mat' folder relative to the current script
-    eng.addpath(os.path.join(root_path, 'mat', 'for_spkd_pw'))  # Add the 'for_spkd_pw' subfolder
 
-    mat_file_path = os.path.join(root_path, 'mat', 'for_spkd_pw', 'for_spkd_pw.mat')
+    mat_file_path = os.path.join(root_path, 'mat', 'spkd_vars.mat')
 
     sio.savemat(mat_file_path, {'cspks': cspks})
-    Dists = eng.py_to_spkd_pw()
+    Dists = np.array(eng.py_to_spkd_pw('debug'))
     eng.quit()
     return Dists
 
