66e8508039394bcab3d3810c731eb04ba175a0f4
NeuroPyPy <Flynnoconnell@gmail.com>
2023-05-30 17:49:03 -0400

    more intuitive naming of spike train distance calculations

--- mat/py_to_spkd_pw.m
+++ mat/py_to_spkd_pw.m
@@ -1,20 +1,47 @@
-function [ D ] = py_to_spkd_pw()
+function [ D ] = py_to_spkd_pw( varargin )
+
+% Check if 'debug' argument is provided
+if nargin > 0 && strcmpi(varargin{1}, 'debug')
+    debugMode = true;
+else
+    debugMode = false;
+end
 
 % Get the directory of the script file
 scriptPath = fileparts(mfilename('fullpath'));
 
-% Add necessary paths relative to the script file
-addpath(fullfile(scriptPath, 'for_spkd_pw')); % Add the 'for_spkd_pw' subfolder
-addpath(fullfile(scriptPath, '..', '..', 'STA')); % Add the 'STA' folder
-addpath(genpath(fullfile(scriptPath, '..', '..', 'STA'))); % Add subfolders of 'STA'
+% Get the parent directory of the script
+parentPath = fullfile(scriptPath, '..');
+
+% Add the 'STA' folder + subfolders
+staFolder = fullfile(parentPath, 'STA');
+addpath(staFolder);
+
+if debugMode
+    disp(['Added to path: ' staFolder]);
+end
+
+% Add subfolders of 'STA'
+staSubFolders = genpath(staFolder);
+addpath(staSubFolders);
+
+if debugMode
+    disp(['Added to path: ' staSubFolders]);
+end
 
 % Load the data file
-filename = fullfile(scriptPath, 'for_spkd_pw', 'for_spkd_pw.mat');
+filename = fullfile(scriptPath, 'spkd_vars.mat');
 loadedVars = load(filename);
 if ~isfield(loadedVars, 'cspks')
     error('Variable cspks does not exist in the file');
 end
 
 cspks = loadedVars.cspks;
+
+if isstruct(cspks)
+    cspks = struct2cell(cspks);
+end
+
+disp(class(cspks))
 
 D = spkd_pw_py(cspks);
