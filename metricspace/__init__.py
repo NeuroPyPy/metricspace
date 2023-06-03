241f61bda93156cee601fab31e4521c6f233a2e9
Flynn <FlynnOConnell@gmail.com>
2023-06-03 19:01:15 -0400

    *fix sliding window now properly updating: unindentation mismatch

--- metricspace/__init__.py
+++ metricspace/__init__.py
@@ -1,4 +1,4 @@
 from .model import *
 from .entropy import *
 
-__all__ = ['distclust', 'spkd', 'spkd_slide', 'histinfo', 'histjabi', 'histbi', 'tblxbi', 'histtpbi', 'tblxtpbi']
+__all__ = ['distclust', 'spkd', 'spkd_slide', 'histinfo', 'histjabi', 'histbi', 'tblxbi', 'histtpbi', 'tblxtpbi', 'tblxinfo']
